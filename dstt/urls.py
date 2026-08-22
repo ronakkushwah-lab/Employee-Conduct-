e8226d658c50d7bb6a9db38e9aa29d20fe017b5a
Deployer <deployer@example.com>
2026-08-22 18:19:19 +0530

    Allow serving user-uploaded profile images and media files unconditionally

--- dstt/urls.py
+++ dstt/urls.py
@@ -64,6 +64,9 @@ urlpatterns = [
     path('api/', include('biometric.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+from django.views.static import serve
+from django.urls import re_path
+
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
